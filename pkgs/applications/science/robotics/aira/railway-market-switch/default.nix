53927b4b511b0227db3c8ca4b1a82cbd9ad0b9a1
Alexander Krupenkin <mail@akru.me>
2017-09-24 12:49:47 +0300

    railway-market-switch: 0.1.1 -> 0.2

--- pkgs/applications/science/robotics/aira/railway-market-switch/default.nix
+++ pkgs/applications/science/robotics/aira/railway-market-switch/default.nix
@@ -4,18 +4,19 @@ with haskellPackages;
 
 mkDerivation {
   pname = "railway-market-switch";
-  version = "0.1.1";
+  version = "0.2";
 
   isLibrary = false;
   isExecutable = true;
 
-  libraryHaskellDepends = with pkgs; [ base web3 z21 miku warp queue transformers ]; 
+  libraryHaskellDepends = with pkgs;
+  [ base web3 z21 miku warp text queue logging transformers ];
 
   src = fetchFromGitHub {
     owner = "airalab";
     repo = "railway-market-switch";
-    rev = "0.1.1";
-    sha256 = "00scgzrjn7m0dzx4fs52zjrad4g9bqkiwggb9s2wxbxrl1a107jg";
+    rev = "0.2";
+    sha256 = "1aqs2hba3nzsvzzy3wmkbyzxbw0zxkdznljdl8br80dpvkhkdwxg";
   };
   
   homepage = http://github.com/airalab/railway-market-switch;
