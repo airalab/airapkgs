784285d6d5fc092e77592ca45fa25917a4e6a9fa
nschoe <nschoe@protonmail.com>
2019-06-06 00:30:14 +0200

    Add dependencies for rviz

--- pkgs/development/ros-modules/rviz/default.nix
+++ pkgs/development/ros-modules/rviz/default.nix
@@ -24,7 +24,16 @@
 , std_srvs
 , tf
 , urdf
+, urdfdom_headers
+, urdfdom
 , visualization_msgs
+, pkgconfig
+, assimp
+, ogre
+, libGLU
+, qt5Full
+, libyamlcpp
+, x11
 }:
 
 let
@@ -42,7 +51,9 @@ in mkRosPackage {
     sha256 = "1cdyylippwirr2ga62zqgyxym95hy66x0mlbqwncl5w9pw3b7ymm";
   };
 
-  propagatedBuildInputs = [ catkin cmake_modules geometry_msgs image_transport interactive_markers laser_geometry map_msgs media_export message_filters nav_msgs pluginlib python_qt_binding resource_retriever rosbag rosconsole roscpp roslib rospy sensor_msgs std_msgs std_srvs tf urdf visualization_msgs ];
+  nativeBuildInputs = [ pkgconfig ];
+
+  propagatedBuildInputs = [ catkin cmake_modules geometry_msgs image_transport interactive_markers laser_geometry map_msgs media_export message_filters nav_msgs pluginlib python_qt_binding resource_retriever rosbag rosconsole roscpp roslib rospy sensor_msgs std_msgs std_srvs tf visualization_msgs urdf urdfdom_headers urdfdom assimp ogre libGLU qt5Full libyamlcpp x11 ];
 
   meta = with stdenv.lib; {
     description = "3D visualization tool for ROS.";
