6e7dd3f9da8b425cee6d191aae3837566950e5fa
nschoe <nschoe@protonmail.com>
2019-06-06 01:05:17 +0200

    Add python dependencies

--- pkgs/development/ros-modules/rviz/default.nix
+++ pkgs/development/ros-modules/rviz/default.nix
@@ -29,17 +29,19 @@
 , visualization_msgs
 , pkgconfig
 , assimp
-, ogre
+, ogre1_9
 , libGLU
 , qt5Full
 , libyamlcpp
 , x11
+, pythonPackages
 }:
 
 let
   pname = "rviz";
   version = "1.13.3";
   rosdistro = "melodic";
+  pythonEnv = pythonPackages.python.withPackages (ps: [ ps.sip ps.pyqt5 ]);
 
 in mkRosPackage {
   name = "${pname}-${version}";
@@ -53,7 +55,9 @@ in mkRosPackage {
 
   nativeBuildInputs = [ pkgconfig ];
 
-  propagatedBuildInputs = [ catkin cmake_modules geometry_msgs image_transport interactive_markers laser_geometry map_msgs media_export message_filters nav_msgs pluginlib python_qt_binding resource_retriever rosbag rosconsole roscpp roslib rospy sensor_msgs std_msgs std_srvs tf visualization_msgs urdf urdfdom_headers urdfdom assimp ogre libGLU qt5Full libyamlcpp x11 ];
+  buildInputs = [ pythonEnv ];
+
+  propagatedBuildInputs = [ catkin cmake_modules geometry_msgs image_transport interactive_markers laser_geometry map_msgs media_export message_filters nav_msgs pluginlib python_qt_binding resource_retriever rosbag rosconsole roscpp roslib rospy sensor_msgs std_msgs std_srvs tf visualization_msgs urdf urdfdom_headers urdfdom assimp ogre1_9 libGLU qt5Full libyamlcpp x11 ];
 
   meta = with stdenv.lib; {
     description = "3D visualization tool for ROS.";
